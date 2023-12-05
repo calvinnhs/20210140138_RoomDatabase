96d66dfd9453865c6d2b3d1aa5cae6442b85fda1
Calvin Hendrawan <tugasakun22@gmail.com>
2023-12-05 20:32:05 +0700

    menambah column pada data siswa

--- app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/roomsiswa/ui/halaman/HalamanHome.kt
@@ -1,5 +1,6 @@
 package com.example.roomsiswa.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -131,7 +132,12 @@ fun DataSiswa(
         modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ) {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ) {
 
+        }
     }
 }
     
